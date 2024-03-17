4fdf0c1c3fb9e9e435779ac289638a8e4472bc16
Pontus Ahlbäck <pontus.ahlback93@gmail.com>
2024-03-17 09:57:31 +0100

    Update education cards, finish education component under resumé

--- cvsida-scroll-react/src/components/Resume.jsx
+++ cvsida-scroll-react/src/components/Resume.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 
 import ExperienceCard from './ExperienceCard'
+import EducationCard from './EducationCard'
 
 const Resume = () => {
   return (
-    <section className='resume'>
-        <h2>RESUMÉ</h2>
-        <h3>EXPERIENCE</h3>
-        <button className='full-resume-button'><h6>Click here for my full resumé</h6></button>
-        <section className='work-experience'>
-          <ExperienceCard />
-        </section>
+    <>
+      <section className='resume experience'>
+          <h2>RESUMÉ</h2>
+          <h3>EXPERIENCE</h3>
+          <button className='full-resume-button'><h6>Click here for my full resumé</h6></button>
+          <section className='work-experience'>
+            <ExperienceCard />
+          </section>
+      </section>
 
-    </section>
+      <section className='resume education'>
+        <h3>EDUCATION</h3>
+        <button className='full-resume-button'><h6>Click here for my full resumé</h6></button>
+        <section className='work-experience education-cards'>
+          <EducationCard />
+        </section>
+      </section>
+
+    </>
+    
   )
 }
 
