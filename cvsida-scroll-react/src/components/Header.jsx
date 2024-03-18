2f280ea053867274ce6c210b742513d2cdc567ac
Pontus Ahlbäck <pontus.ahlback93@gmail.com>
2024-03-18 12:38:40 +0100

    update header - hide scroll nav when not on root

--- cvsida-scroll-react/src/components/Header.jsx
+++ cvsida-scroll-react/src/components/Header.jsx
@@ -1,26 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FaAlignJustify } from "react-icons/fa6";
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { Link } from 'react-scroll';
 
 import './Header.css'
 
 const Header = () => {
   const [hamburgerOpen, setHamburgerOpen] = useState(false);
+  const [homeOpen, setHomeOpen] = useState(false);
+  const homeRoute = "/";
+  const location = useLocation();
 
   function toggleHamburger() {
     setHamburgerOpen(!hamburgerOpen)
   };
 
+  useEffect(() => {
+    if (location.pathname === homeRoute){
+      setHomeOpen(true);
+    } else {
+      setHomeOpen(false);
+    }
+  }, [location.pathname, homeRoute])
+
   return (
     <>
         <header className='page-header'>
             <nav className='navbar' style={{display: `${hamburgerOpen ? 'inline' : 'none'}`}}>
-                <ul>
-                    <li><NavLink to="/" className='scroll-link'>HOME</NavLink></li>
-                    <li><NavLink to="resume" className='scroll-link'>FULL RESUMÉ</NavLink></li>
+                <ul className='primary-nav'>
+                    <li><NavLink to="/" className='navlink'>HOME</NavLink></li>
+                    <li><NavLink to="resume" className='navlink'>FULL RESUMÉ</NavLink></li>
                 </ul>
-                <ul>
+                <div className='navbar-separator' style={{display: `${homeOpen ? 'block' : 'none'}`}}></div>
+                <ul className='secondary-nav' style={{display: `${homeOpen ? 'flex' : 'none'}`}}>
                   <li><Link to="about" smooth duration={500} className='scroll-link'>ABOUT</Link></li>
                   <li><Link to="resume" smooth duration={500} className='scroll-link'>RESUMÉ</Link></li>
                   <li><Link to="projects" smooth duration={500} className='scroll-link'>PROJECTS</Link></li>
